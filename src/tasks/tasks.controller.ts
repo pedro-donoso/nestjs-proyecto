cac33af1ab50b39f44ec928b84806d01485a9367
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-06 16:44:29 -0300

    agrego metodos tareas

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
 import { TasksService } from './tasks.service';
 
 
@@ -12,4 +12,23 @@ export class TaskController {
     return this.tasksService.getTasks();
    }
 
+    @Post('/tasks')
+   createTask(){
+    return 'Creando tareas';
+   }
+
+    @Put('/tasks')
+  updateTask(){
+    return 'Actualizando tareas';
+   }
+
+    @Delete('/tasks')
+  deleteTask(){
+    return 'Eliminando tareas';
+   }
+
+     @Patch('/tasks')
+  updateTaskStatus(){
+    return 'actualizando el estado de una tarea';
+   }
 }
