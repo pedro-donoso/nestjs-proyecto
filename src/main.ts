d18b34fae2e8952a8836d09bc823943ad4a830ec
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-06 17:09:51 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { serveStatic } from 'serve-static';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Servir archivos estáticos desde el directorio 'public'
+  app.use(serveStatic(join(__dirname, '..', 'public')));
+
+  await app.listen(3000);
 }
 bootstrap();
