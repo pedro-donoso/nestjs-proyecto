77c0eda9fe4a410fecf0cdeab0d29f20cc801a77
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-06 14:07:51 -0300

    creo module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TaskModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TaskModule, ProjectsModule, AuthModule],
+  imports: [TaskModule, ProjectsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
